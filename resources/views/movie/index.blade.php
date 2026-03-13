a3e78d8125405ad562c1625391297536eb2c4f32
princess_wawa <princess_wawa@proton.me>
2026-03-13 13:52:06 +0100

    renome tout les fichier fonctions et variables pour notre site de films

---
+++ resources/views/movie/index.blade.php
@@ -0,0 +1,11 @@
+<h1>Liste des Films</h1>
+
+<ul>
+@foreach($movies as $movie)
+    <li>
+        <a href="/movies/{{ $movie['id'] }}">
+            {{ $movie['title'] }}
+        </a>
+    </li>
+@endforeach
+</ul>
