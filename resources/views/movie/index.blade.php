3d2a207ae693bcea25ba17412a1cd0f7bcc5ae0b
princess_wawa <princess_wawa@proton.me>
2026-03-13 15:54:04 +0100

    add styles for the index

--- resources/views/movie/index.blade.php
+++ resources/views/movie/index.blade.php
@@ -3,13 +3,14 @@
 @section('content')
 <h1>Liste des Films</h1>
 
-<ul>
+<dir class="cards-container">
 @foreach($movies as $movie)
-    <li>
-        <a href="/movies/{{ $movie['id'] }}">
-            {{ $movie['title'] }}
-        </a>
-    </li>
+    <div class="card" onclick="window.location='{{ url('/movies/' . $movie['id']) }}'">
+        <div class="card-image">
+            <img src="{{ asset('Image/films/' . $movie['Images']) }}">
+        </div>
+        <p class="card-title">{{ $movie['title'] }}</p>
+    </div>
 @endforeach
-</ul>
+</dir>
 @endsection
