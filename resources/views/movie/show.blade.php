bc7b2a1dcfc4b9d5815bad800eb4e92e619443b9
princess_wawa <none>
2026-06-01 15:59:51 +0200

    ajout du boutton '+' pour ajouter des films

--- resources/views/movie/show.blade.php
+++ resources/views/movie/show.blade.php
@@ -1,27 +1,30 @@
 @extends('layout.app')
 
 @section('content')
-    
+
 <div class="movie-container">
+    
+    <div class="buttons-container">
 
-    <!-- Actions -->
-    <div class="movie-actions">
-
-        <!-- Edit -->
-        <a class="icon-btn edit-btn" href="/edit/{{ $movie['id'] }}" title="Modifier">
-            <!-- Pen icon -->
-            <svg viewBox="0 0 24 24">
-                <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25z"></path>
-                <path d="M20.71 7.04a1 1 0 0 0 0-1.41l-2.34-2.34a1 1 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path>
-            </svg>
-        </a>
-
-        <!-- Delete -->
-        <form action="{{ route('movies.delete', $movie['id']) }}" method="POST"
-              onsubmit="return confirm('Supprimer ce film ?')">
+        <a class="back-btn" href="/movies">Retour</a>
+        <!-- Actions -->
+        <div class="movie-actions">
+            
+            <!-- Edit -->
+            <a class="icon-btn edit-btn" href="/edit/{{ $movie['id'] }}" title="Modifier">
+                <!-- Pen icon -->
+                <svg viewBox="0 0 24 24">
+                    <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25z"></path>
+                    <path d="M20.71 7.04a1 1 0 0 0 0-1.41l-2.34-2.34a1 1 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path>
+                </svg>
+            </a>
+            
+            <!-- Delete -->
+            <form action="{{ route('movies.delete', $movie['id']) }}" method="POST"
+            onsubmit="return confirm('Supprimer ce film ?')">
             @csrf
             @method('DELETE')
-
+            
             <button type="submit" class="icon-btn delete-btn" title="Supprimer">
                 <!-- Trash icon -->
                 <svg viewBox="0 0 24 24">
@@ -31,19 +34,21 @@
                 </svg>
             </button>
         </form>
-
+        
+        </div>
     </div>
 
     <h1 class="movie-title">{{ $movie['title'] }}</h1>
 
     <div class="movie-content">
 
+        
         <img class="movie-poster" src="{{ asset('Image/films/' . $movie['images']) }}">
-
+        
         <div class="movie-description">
             <p>{{ $movie['description'] }}</p>
         </div>
-
+        
     </div>
 
 </div>
